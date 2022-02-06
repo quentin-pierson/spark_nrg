571052c245f155cb94e5d1f0ed64ec171a1e4046
pperezdev <pierrick.perez@free.fr>
2022-02-06 22:04:29 +0100

    Add sql script and scala

--- sql/D_TEMP.sql
+++ sql/D_TEMP.sql
@@ -1,7 +1,9 @@
 CREATE TABLE D_TEMP(
-    Production VARCHAR(255),
+    Code VARCHAR(255),
     intitule VARCHAR(255),
     Unite VARCHAR(255),
+    Region VARCHAR(255),
+    Ressource_type VARCHAR(255),
     annee1 FLOAT,
     annee2 FLOAT,
     annee3 FLOAT,
--- sql/init/DATA.sql
+++ sql/init/DATA.sql
@@ -1,7 +1,9 @@
 CREATE TABLE D_TEMP(
-    Production VARCHAR(255),
+    Code VARCHAR(255),
     intitule VARCHAR(255),
     Unite VARCHAR(255),
+    Region VARCHAR(255),
+    Ressource_type VARCHAR(255),
     annee1 FLOAT,
     annee2 FLOAT,
     annee3 FLOAT,
@@ -24,7 +26,7 @@ CREATE TABLE D_REGION(
 );
 
 CREATE TABLE D_SECTEUR_ENERGETIQUE(
-    ID_Secteur_Energetique INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Code_Secteur_Energetique VARCHAR(255) PRIMARY KEY NOT NULL,
     Libelle VARCHAR(255) NOT NULL,
     DELETE_LINE INT DEFAULT 0,
     DT_INSERT TIMESTAMP DEFAULT now(),
@@ -41,21 +43,35 @@ CREATE TABLE D_TYPE_RESSOURCE(
 
 CREATE TABLE D_RESSOURCE(
     ID_Ressource INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    Libelle VARCHAR(255) NOT NULL,
-    Unite_Energetique VARCHAR(255) NOT NULL,
+    Unite_Energetique VARCHAR(255) NULL,
     Valeur_Energetique FLOAT NOT NULL,
     DT_Year DATE NOT NULL,
     FK_Type_Ressource INT NOT NULL,
     FK_Region INT NOT NULL,
-    FK_Secteur_Energetique INT NOT NULL,
+    FK_Secteur_Energetique VARCHAR(255) NOT NULL,
     DELETE_LINE INT DEFAULT 0,
     DT_INSERT TIMESTAMP DEFAULT now(),
     DT_UPDATE TIMESTAMP DEFAULT now(),
     FOREIGN KEY (DT_Year) REFERENCES D_DATE(DT_Year),
     FOREIGN KEY (FK_Type_Ressource) REFERENCES D_TYPE_RESSOURCE(ID_Type_Ressource),
     FOREIGN KEY (FK_Region) REFERENCES D_REGION(ID_Region),
-    FOREIGN KEY (FK_Secteur_Energetique) REFERENCES D_SECTEUR_ENERGETIQUE(ID_Secteur_Energetique)
+    FOREIGN KEY (FK_Secteur_Energetique) REFERENCES D_SECTEUR_ENERGETIQUE(Code_Secteur_Energetique)
 );
+
+DELIMITER //
+CREATE PROCEDURE SP_SET_REGION()
+    BEGIN
+
+    MERGE D_REGION AS DST
+    USING (SELECT DISTINCT Region AS LB_REGION 
+            FROM D_TEMP AS TMP)
+        AS SRC
+        ON DST.Libelle = SRC.LB_REGION
+    WHEN NOT MATCHED THEN
+    INSERT (LB_REGION)
+    VALUES (SRC.LB_REGION)
+    END //
+DELIMITER ;
 
 INSERT INTO D_DATE (`DT_Year`, `Libelle_Year`) VALUES ('2014-01-01', '2014'),
                         ('2015-01-01', '2015'),
